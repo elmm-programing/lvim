8eba9d45d5b7e30ca54c60c70c389717724aebe5
edmejia <edmejia@hacienda.gov.do>
2024-07-30 15:04:12 -0400

    work configuration and volar downgrade

--- lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
-M = {}
-lvim.leader = ","
-
-local opts = { noremap = true, silent = true }
-local keymap = vim.keymap.set
-keymap("i", "kj", "<Esc>", opts)
-
-
--- lua/lang/java.lua
+++
@@ -1,4 +0,0 @@
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
-lvim.builtin.treesitter.ensure_installed = {
-  "java",
-}

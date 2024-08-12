5b0fb9336bdf7a950f0f431d85efcca72ea1347e
ELMM <elmm.programing@gmail.com>
2024-08-12 19:51:25 -0400

    Copilot chat plugin added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,6 +2,19 @@
 lvim.plugins = {
   "catppuccin/nvim",
   "hashivim/vim-terraform",
+   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   {
     "mawkler/modicator.nvim",
     event = "ColorScheme",
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -3,12 +3,56 @@ lvim.builtin.which_key.setup.ignore_missing = true
 
 lvim.builtin.which_key.mappings["b"] = { "<cmd>Telescope buffers<cr>", "Buffers" }
 lvim.builtin.which_key.mappings["v"] = { "<cmd>vsplit<cr>", "vsplit" }
-lvim.builtin.which_key.mappings["h"] = { "<cmd>nohlsearch<cr>", "nohl" }
 lvim.builtin.which_key.mappings["o"] = { "<cmd>AerialToggle!<cr>", "Outline" }
 lvim.builtin.which_key.mappings["q"] = { '<cmd>lua require("user.functions").smart_quit()<CR>', "Quit" }
 lvim.builtin.which_key.mappings["/"] = { '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', "Comment" }
 -- lvim.builtin.which_key.mappings["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" }
 lvim.builtin.which_key.mappings["gy"] = "Link"
+lvim.builtin.which_key.mappings["h"] = {
+  name = "Copilot Chat",
+  o = { "<cmd>CopilotChatOpen<cr>", "Open Chat" },
+  c = { "<cmd>CopilotChatClose<cr>", "Close Chat" },
+  t = { "<cmd>CopilotChatToggle<cr>", "Toggle Chat" },
+  s = { "<cmd>CopilotChatSend<cr>", "Send Message" },
+  r = { "<cmd>CopilotChatReply<cr>", "Reply to Message" },
+  h = { "<cmd>CopilotChatHistory<cr>", "Chat History" },
+  d = {
+    name = "advance",
+    c = { "<cmd>CopilotChat<cr>", "Open Chat Window" },
+    o = { "<cmd>CopilotChatOpen<cr>", "Open Chat Window" },
+    x = { "<cmd>CopilotChatClose<cr>", "Close Chat Window" },
+    t = { "<cmd>CopilotChatToggle<cr>", "Toggle Chat Window" },
+    s = { "<cmd>CopilotChatStop<cr>", "Stop Copilot Output" },
+    r = { "<cmd>CopilotChatReset<cr>", "Reset Chat Window" },
+    S = { "<cmd>CopilotChatSave<cr>", "Save Chat History" },
+    l = { "<cmd>CopilotChatLoad<cr>", "Load Chat History" },
+    d = { "<cmd>CopilotChatDebugInfo<cr>", "Show Debug Info" },
+    m = { "<cmd>CopilotChatModels<cr>", "View and Select Models" },
+    e = { "<cmd>CopilotChatExplain<cr>", "Explain Code" },
+    v = { "<cmd>CopilotChatReview<cr>", "Review Code" },
+    f = { "<cmd>CopilotChatFix<cr>", "Fix Code" },
+    O = { "<cmd>CopilotChatOptimize<cr>", "Optimize Code" },
+    D = { "<cmd>CopilotChatDocs<cr>", "Add Documentation" },
+    T = { "<cmd>CopilotChatTests<cr>", "Generate Tests" },
+    F = { "<cmd>CopilotChatFixDiagnostic<cr>", "Fix Diagnostic Issue" },
+    C = { "<cmd>CopilotChatCommit<cr>", "Write Commit Message" },
+    p = { "<cmd>CopilotChatCommitStaged<cr>", "Write Commit Message for Staged Changes" },
+
+  }
+
+}
+lvim.builtin.which_key.mappings["c"] = {
+  name = "Copilot",
+  e = { "<cmd>Copilot enable<cr>", "Enable Copilot" },
+  d = { "<cmd>Copilot disable<cr>", "Disable Copilot" },
+  s = { "<cmd>Copilot status<cr>", "Copilot Status" },
+  p = { "<cmd>Copilot panel<cr>", "Open Copilot Panel" },
+  n = { "<cmd>Copilot next<cr>", "Next Suggestion" },
+  m = { "<cmd>Copilot prev<cr>", "Previous Suggestion" },
+  a = { "<cmd>Copilot accept<cr>", "Accept Suggestion" },
+}
+
+
 lvim.builtin.which_key.mappings["r"] = {
   name = "Replace",
   r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
@@ -166,12 +210,12 @@ lvim.builtin.which_key.mappings["s"] = nil
 lvim.builtin.which_key.mappings["w"] = nil
 
 local m_opts = {
-  mode = "n", -- NORMAL mode
+  mode = "n",     -- NORMAL mode
   prefix = "m",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
+  buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true,  -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
+  nowait = true,  -- use `nowait` when creating keymaps
 }
 
 local status_ok, which_key = pcall(require, "which-key")
